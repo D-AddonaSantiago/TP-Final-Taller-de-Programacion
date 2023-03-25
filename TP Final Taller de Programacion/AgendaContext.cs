e141791fb74fb0b78f305cbb440aecdb0390019a
santiago <santiagodaddona@gmail.com>
2023-03-25 10:08:19 -0300

    Agregar archivos de proyecto.

---
+++ TP Final Taller de Programacion/AgendaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Final_Taller_de_Programacion
+{
+    public class AgendaContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+
+        public DbSet<Telefono> Telefonos { get; set; }
+    }
+}
---
+++ TP Final Taller de Programacion/Telefono.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Final_Taller_de_Programacion
+{
+    public class Telefono
+    {
+        public int TelefonoId { get; set; }
+
+        public string Numero { get; set; }
+
+        public string Tipo { get; set; }
+    }
+}
